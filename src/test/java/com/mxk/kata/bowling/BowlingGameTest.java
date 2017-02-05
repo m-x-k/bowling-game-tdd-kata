79f995cf8649b73b5a14d7d80ed4160fe6db5266
martykells <martykells@gmail.com>
2017-02-05 16:52:40 +0000

    Refactor tests to use test setup instance

--- src/test/java/com/mxk/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/mxk/kata/bowling/BowlingGameTest.java
@@ -1,14 +1,21 @@
 package com.mxk.kata.bowling;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class BowlingGameTest {
 
+    private BowlingGame g;
+
+    @Before
+    public void setup() throws Exception {
+        g = new BowlingGame();
+    }
+
     @Test
     public void testGutterGame() throws Exception {
-        BowlingGame g = new BowlingGame();
         for (int i=0; i<20; i++) {
             g.roll(0);
         }
@@ -17,7 +24,6 @@ public class BowlingGameTest {
 
     @Test
     public void testAllOnes() throws Exception {
-        BowlingGame g = new BowlingGame();
         for (int i = 0; i < 20; i++) {
             g.roll(1);
         }

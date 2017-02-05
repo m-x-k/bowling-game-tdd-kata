c3d4faf21603406884488c20f6b6d0ee7d7d9080
martykells <martykells@gmail.com>
2017-02-05 16:49:42 +0000

    Test all ones

--- src/main/java/com/mxk/kata/bowling/BowlingGame.java
+++ src/main/java/com/mxk/kata/bowling/BowlingGame.java
@@ -2,11 +2,13 @@ package com.mxk.kata.bowling;
 
 public class BowlingGame {
 
-    public void roll(int i) {
+    private int score = 0;
 
+    public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
--- src/test/java/com/mxk/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/mxk/kata/bowling/BowlingGameTest.java
@@ -11,8 +11,16 @@ public class BowlingGameTest {
         BowlingGame g = new BowlingGame();
         for (int i=0; i<20; i++) {
             g.roll(0);
-            assertEquals(0, g.score());
         }
+        assertEquals(0, g.score());
+    }
 
+    @Test
+    public void testAllOnes() throws Exception {
+        BowlingGame g = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            g.roll(1);
+        }
+        assertEquals(20, g.score());
     }
 }

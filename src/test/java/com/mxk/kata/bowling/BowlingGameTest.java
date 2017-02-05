e3401830bfcb9d0ad561163a7eaad127607e53a4
martykells <martykells@gmail.com>
2017-02-05 16:54:45 +0000

    Refactor tests to use rollMany loop

--- src/test/java/com/mxk/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/mxk/kata/bowling/BowlingGameTest.java
@@ -16,18 +16,18 @@ public class BowlingGameTest {
 
     @Test
     public void testGutterGame() throws Exception {
-        rollMany(0);
+        rollMany(20,0);
         assertEquals(0, g.score());
     }
 
     @Test
     public void testAllOnes() throws Exception {
-        rollMany(1);
+        rollMany(20,1);
         assertEquals(20, g.score());
     }
 
-    private void rollMany(int pins) {
-        for (int i = 0; i < 20; i++) {
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
             g.roll(pins);
         }
     }

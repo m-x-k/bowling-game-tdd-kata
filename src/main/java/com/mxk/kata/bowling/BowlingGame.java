9caec79b149d62abfc304e860c5ea92d66e8cf24
martykells <martykells@gmail.com>
2017-02-05 16:46:06 +0000

    Gutter test: rolls a zero score

--- src/main/java/com/mxk/kata/bowling/BowlingGame.java
+++ src/main/java/com/mxk/kata/bowling/BowlingGame.java
@@ -1,4 +1,12 @@
 package com.mxk.kata.bowling;
 
 public class BowlingGame {
+
+    public void roll(int i) {
+
+    }
+
+    public int score() {
+        return 0;
+    }
 }
--- src/test/java/com/mxk/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/mxk/kata/bowling/BowlingGameTest.java
@@ -2,10 +2,17 @@ package com.mxk.kata.bowling;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BowlingGameTest {
 
     @Test
     public void testGutterGame() throws Exception {
         BowlingGame g = new BowlingGame();
+        for (int i=0; i<20; i++) {
+            g.roll(0);
+            assertEquals(0, g.score());
+        }
+
     }
 }
